e39b7bd322f4cbf2ad4d9f0634ff6498cd7469fe
Manar saber salah <123212896+manarsaber2@users.noreply.github.com>
2025-09-21 19:19:28 +0300

    Add files via upload

---
+++ Who Has the Most Friends.sql
@@ -0,0 +1,11 @@
+SELECT user_id AS id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS user_id, accepter_id AS friend_id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS user_id, requester_id AS friend_id
+    FROM RequestAccepted
+) AS friendships
+GROUP BY user_id
+ORDER BY num DESC
+LIMIT 1;
